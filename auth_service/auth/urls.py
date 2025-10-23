507f95da96afd448ceee71ac739c33515fcab789
DKinKOR <kdw8219@gmail.com>
2025-10-23 15:20:41 +0900

    Fix runnable and token management (#2)
    * Feature
     - change url
     - cache management logic develop
    
    * Feature
     - Data that should not be opened is encapsulated

--- auth_service/auth/urls.py
+++ auth_service/auth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from auth import views as auth_views
 
 urlpatterns = [
-    path('api/tokens/', auth_views.token_view, name='auth_login'), # POST/DELETE
-    path('api/tokens/refresh/', auth_views.token_refresh, name='auth_refresh'), # POST
+    path('', auth_views.token_view, name='auth_login'), # POST/DELETE
+    path('refresh/', auth_views.token_refresh, name='auth_refresh'), # POST
 ]
--- auth_service/tokens/views.py
+++ auth_service/tokens/views.py
@@ -1,17 +1,61 @@
 from django.core.cache import cache
+import jwt
+import datetime
+import os
+from dotenv import load_dotenv
+from django_redis import get_redis_connection
 
 def is_token_valid(token):
-    """
-    Check if the given token is valid (not blacklisted).
-    """
-    blacklisted_tokens = cache.get('blacklisted_tokens', set())
-    return token not in blacklisted_tokens
+    conn = get_redis_connection("default")
+    key = conn.get(f'blacklist_{token}', None)
+    
+    return token == key if True else False
 
-def create_new_tokens(user_id):
+def add_token_to_blacklist(token):
+    conn = get_redis_connection("default")
+    res = conn.set(f'blacklist_{token}', 1, ex = 10*24*60*60)  # 10일 동안 블랙리스트에 저장
+    
+    if res == None:
+        print("add token to blacklist success")
+
+def is_in_refresh_token(token):
+    conn = get_redis_connection("default")
+    key = conn.get(f'{token}', None)
+    
+    return token == key if True else False
+
+def delete_token_from_refresh_token(token):
+    conn = get_redis_connection("default")
+    conn.delete(f'{token}') #after delete, don't care anything.
+
+def create_new_tokens():
     """
     Create new access and refresh tokens for the given user_id.
     This is a placeholder function. Implement token creation logic here.
     """
-    new_access_token = f"access_token_for_{user_id}"
-    new_refresh_token = f"refresh_token_for_{user_id}"
+    
+    load_dotenv()
+    ACCESS_SECRET_KEY = os.getenv("ACCESS_SECRET_KEY")
+    REFRESH_SECRET_KEY = os.getenv("REFRESH_SECRET_KEY")
+    ISS = os.getenv("ISS")
+    
+    payload = {
+        "iss": ISS,
+        "username": ISS,
+        "exp": datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=5),
+        "iat": datetime.datetime.now(datetime.timezone.utc)
+    }
+    new_access_token = jwt.encode(payload, ACCESS_SECRET_KEY, algorithm="HS256")
+    
+    payload = {
+        "iss": ISS,
+        "username": ISS,
+        "exp": datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=7),
+        "iat": datetime.datetime.now(datetime.timezone.utc)
+    }
+    new_refresh_token = jwt.encode(payload, REFRESH_SECRET_KEY, algorithm="HS256")
+    
+    conn = get_redis_connection("default")
+    conn.add(new_refresh_token, 1, ex = 7*24*60*60)  # 7일 동안 refresh token에 저장
+    
     return new_access_token, new_refresh_token
