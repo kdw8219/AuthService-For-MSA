79616317e79927fe06804825d90b719b2f1e91b4
kdw8219 <kdw8219@gmail.com>
2025-10-27 23:27:07 +0900

    Feature  - Test code --> auth working check complete  - need to test refresh validation check function

--- auth_service/auth/tests.py
+++ auth_service/auth/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+
+class auth_view_test(TestCase):
+    def setup(self):
+        self.client = Client()
+    
+    def test_token_view(self):
+        response = self.client.post(reverse(''),{
+            'username': 'test',
+            'password': '1234'
+        })
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '')
--- auth_service/auth/urls.py
+++ auth_service/auth/urls.py
@@ -4,4 +4,5 @@ from auth import views as auth_views
 urlpatterns = [
     path('', auth_views.token_view, name='auth_login'), # POST/DELETE
     path('refresh/', auth_views.token_refresh, name='auth_refresh'), # POST
+    path('validate/', auth_views.validate_token, name='validate_token') #GET
 ]
